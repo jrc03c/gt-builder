f7097785fd0f8e40b3e2a61e35143dbc55e3ba19
jrc03c <jrc03c@pm.me>
2026-04-02 11:20:10 -0500

    ...

--- src/bin.js
+++ src/bin.js
@@ -6,26 +6,6 @@ import fs from "node:fs"
 import path from "node:path"
 import process from "node:process"
 
-program
-  .name("gt-build")
-  .option("-d, --dist-dir <dir>")
-  .option("-w, --watch")
-  .argument("[paths...]")
-
-program.parse()
-
-const options = program.opts()
-const distDir = options.distDir ?? path.join(process.cwd(), "dist")
-const paths = program.args
-
-if (paths.length === 0) {
-  paths.push(process.cwd())
-}
-
-for (let i = 0; i < paths.length; i++) {
-  paths[i] = path.resolve(paths[i])
-}
-
 function debounce(fn, ms) {
   let timeout
 
@@ -35,40 +15,73 @@ function debounce(fn, ms) {
   }
 }
 
-const build = debounce(() => {
-  console.log("-".repeat(80))
-  console.log(`Building... (${new Date().toLocaleTimeString()})`)
+program.name("gt-builder")
 
-  try {
+program.command("init [dest]").action(dest => {
+  const src = path.join(import.meta.dirname, "..", "template")
+  dest = path.resolve(dest ?? process.cwd())
+
+  if (!fs.existsSync(dest)) {
+    fs.mkdirSync(dest, { recursive: true })
+  }
+
+  fs.cpSync(src, dest, { recursive: true })
+  console.log(`Initialized program template: ${dest}`)
+})
+
+program
+  .command("build [paths...]")
+  .option("-d, --dist-dir <dir>")
+  .option("-e, --exclude <paths...>")
+  .option("-w, --watch")
+  .action((paths, options) => {
+    const distDir = options.distDir ?? path.join(process.cwd(), "dist")
+
+    if (paths.length === 0) {
+      paths.push(process.cwd())
+    }
+
     for (let i = 0; i < paths.length; i++) {
-      const p = paths[i]
+      paths[i] = path.resolve(paths[i])
+    }
 
-      const srcDir = fs.statSync(p).isDirectory()
-        ? p
-        : p.split(path.sep).slice(0, -1).join(path.sep)
+    const build = debounce(() => {
+      console.log("-".repeat(80))
+      console.log(`Building... (${new Date().toLocaleTimeString()})`)
 
-      const builder = new GTBuilder({
-        distDir,
-        srcDir,
-      })
+      try {
+        for (let i = 0; i < paths.length; i++) {
+          const p = paths[i]
 
-      const outfiles = builder.build()
+          const srcDir = fs.statSync(p).isDirectory()
+            ? p
+            : p.split(path.sep).slice(0, -1).join(path.sep)
 
-      for (const f of outfiles) {
-        console.log(`Built: ${f}`)
+          const builder = new GTBuilder({
+            distDir,
+            srcDir,
+          })
+
+          const outfiles = builder.build()
+
+          for (const f of outfiles) {
+            console.log(`Built: ${f}`)
+          }
+        }
+
+        console.log(`Built! 🎉  (${new Date().toLocaleTimeString()})`)
+      } catch (e) {
+        console.error(e)
+      }
+    }, 100)
+
+    if (options.watch) {
+      for (const p of paths) {
+        fs.watch(p, { recursive: true }, () => build())
       }
     }
 
-    console.log(`Built! 🎉  (${new Date().toLocaleTimeString()})`)
-  } catch (e) {
-    console.error(e)
-  }
-}, 100)
+    build()
+  })
 
-if (options.watch) {
-  for (const p of paths) {
-    fs.watch(p, { recursive: true }, () => build())
-  }
-}
-
-build()
+program.parse()
